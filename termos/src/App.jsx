e57d44f208e4b82e544e490023a39f8ffe245d90
Gabriel Ferreira da Silva <gabrielbiel.ferreira0411@gmail.com>
2025-03-19 18:49:04 -0300

    first commit

---
+++ termos/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Termos from './components/Termos';
+
+const App = () => {
+    return (
+        <div>
+            <Termos />
+        </div>
+    );
+};
+
+export default App;
