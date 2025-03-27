2bf1008f0e1dc785bd636e3bc3c5990fc495211f
Gabriel Ferreira da Silva <gabrielbiel.ferreira0411@gmail.com>
2025-03-27 17:01:23 -0300

    Adicionando projeto React Login

---
+++ sobre/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sobre from './components/Sobre'; // Importe o componente Sobre
+
+const App = () => {
+    return (
+        <div>
+            <Sobre /> {/* Renderize o componente Sobre diretamente */}
+        </div>
+    );
+};
+
+export default App;
