a33859718c6b89a5f3aeef2126ffa02d01df6c88
ClaudiaKRibeiro <claudiakele6@gmail.com>
2025-03-27 22:01:05 -0300

    Primeiro commit orientações

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Orientacoes from './components/paginaOrientacoes/paginaOrientacoes'
+import './index.css'
+
+function App() {
+  
+  return (
+    <>
+      
+    <Orientacoes />
+    
+    </>
+  )
+}
+
+export default App
