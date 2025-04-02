c23e1983e0e1c3980b69ebc65a4c26d5a972b2ca
Maria Gabriella <mgsc@cesar.school>
2025-04-01 21:30:05 -0300

    Padronizando o footer e suas classes

--- front/src/components/footer/Footer.jsx
+++ front/src/components/footer/Footer.jsx
@@ -1,42 +1,41 @@
 import { Link } from "react-router-dom";
 import "./footer.css"; // Importa o CSS específico para o footer
+
 function Footer() {
   return (
-    <footer className="app-footer">
-      <div className="container">
-        <div className="footer-container">
-          <div className="footer-brand">
-            <Link to="/">
-              <img src="./logo.png" alt="Logo" className="logo" />
-            </Link>
-          </div>
-          <div className="footer-section">
-            <h3>Links úteis</h3>
-            <ul className="footer-links">
-              <li>
-                <Link to="/sobre">Sobre nós</Link>
-              </li>
-              <li>
-                <Link to="/mapa">Mapa de alerta</Link>
-              </li>
-              <li>
-                <Link to="/orientacoes">Orientações</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-section">
-            <h3>Políticas</h3>
-            <ul className="footer-links">
-              <li>
-                <Link to="/termos">Termos e Condições</Link>
-              </li>
-            </ul>
-          </div>
+    <footer className="alerta-risk-footer">
+      <div className="alerta-risk-footer-container">
+        <div className="alerta-risk-footer-brand">
+          <Link to="/">
+            <img src="./logo.png" alt="Logo AlertaRisk" className="alerta-risk-logo" />
+          </Link>
         </div>
-        <div className="divider"></div>
-        <div className="copyright">
-          AlertaRisk. Alguns direitos reservados.
+        <div className="alerta-risk-footer-section">
+          <h3>Links úteis</h3>
+          <ul className="alerta-risk-footer-links">
+            <li>
+              <Link to="/sobre">Sobre nós</Link>
+            </li>
+            <li>
+              <Link to="/mapa">Mapa de alerta</Link>
+            </li>
+            <li>
+              <Link to="/orientacoes">Orientações</Link>
+            </li>
+          </ul>
         </div>
+        <div className="alerta-risk-footer-section">
+          <h3>Políticas</h3>
+          <ul className="alerta-risk-footer-links">
+            <li>
+              <Link to="/termos">Termos e Condições</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="alerta-risk-footer-divider"></div>
+      <div className="alerta-risk-footer-copyright">
+        AlertaRisk. Alguns direitos reservados.
       </div>
     </footer>
   );
