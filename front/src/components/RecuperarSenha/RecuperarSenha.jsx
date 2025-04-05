6623d114b3f44ed0466a93e983201c8f77f8f987
Maria Gabriella <mgsc@cesar.school>
2025-04-05 14:59:34 -0300

    Header e Recuperação senha atualizado

--- front/src/components/RecuperarSenha/RecuperarSenha.jsx
+++ front/src/components/RecuperarSenha/RecuperarSenha.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "../../pages/cadastro/cadastro.css";
-
 
 const RecuperarSenha = () => {
   const navigate = useNavigate();
